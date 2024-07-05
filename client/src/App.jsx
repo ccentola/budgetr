e98e58ce7a7b11c0e3ad47eb9e790ffc78f904af
Carl Centola <carl.centola@gmail.com>
2024-07-05 07:46:50 -0700

    add token flow logic to app and extracts latest transactions JSON file (resolves #1). enables the connection of a bank using Plaid's Link UI. (resolves #2)

---
+++ client/src/App.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { usePlaidLink } from "react-plaid-link";
+// import './App.css'
+
+axios.defaults.baseURL = "http://localhost:8000";
+
+function PlaidAuth({ publicToken }) {
+  const [account, setAccount] = useState();
+  const [transactions, setTransactions] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      let accessToken = await axios.post("/api/set_access_token", {
+        public_token: publicToken,
+      });
+      console.log("accessToken", accessToken.data);
+      const auth = await axios.post("/api/auth", {
+        access_token: accessToken.data.accessToken,
+      });
+      console.log("auth data ", auth.data);
+      setAccount(auth.data.numbers.ach[0]);
+      const transactions = await axios.post("/api/transactions", {
+        access_token: accessToken.data.accessToken,
+      });
+      console.log("transaction data ", transactions.data);
+      setTransactions(transactions.data);
+    }
+    fetchData();
+  }, []);
+  return (
+    transactions && (
+      <>
+        <p>Account number: {account.account}</p>
+        <p>Routing number: {account.routing}</p>
+      </>
+    )
+  );
+}
+
+function App() {
+  const [linkToken, setLinkToken] = useState();
+  const [publicToken, setPublicToken] = useState();
+
+  useEffect(() => {
+    async function fetch() {
+      const response = await axios.post("/api/create_link_token");
+      setLinkToken(response.data.link_token);
+    }
+    fetch();
+  }, []);
+
+  const { open, ready } = usePlaidLink({
+    token: linkToken,
+    onSuccess: (public_token, metadata) => {
+      setPublicToken(public_token);
+      console.log("success", public_token, metadata);
+      // send public_token to server
+    },
+  });
+
+  return publicToken ? (
+    <PlaidAuth publicToken={publicToken} />
+  ) : (
+    <button onClick={() => open()} disabled={!ready}>
+      Connect a bank account
+    </button>
+  );
+}
+
+export default App;
--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 import os
 import time
 import json
+from fastapi import FastAPI, HTTPException, Request, Body
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
-from typing import List, Optional
-from datetime import date, timedelta
+from dotenv import load_dotenv
 import plaid
 from plaid.api import plaid_api
-from fastapi import FastAPI, HTTPException, Request
-from fastapi.encoders import jsonable_encoder
-from dotenv import load_dotenv
 from plaid.model.products import Products
 from plaid.model.country_code import CountryCode
 from plaid.model.link_token_create_request import LinkTokenCreateRequest
 from plaid.model.link_token_create_request_user import LinkTokenCreateRequestUser
+from plaid.model.transactions_sync_request import TransactionsSyncRequest
+from plaid.model.auth_get_request import AuthGetRequest
+from plaid.model.item_public_token_exchange_request import (
+    ItemPublicTokenExchangeRequest,
+)
+from plaid.model.item_public_token_exchange_response import (
+    ItemPublicTokenExchangeResponse,
+)
+from plaid.exceptions import ApiException
 
 load_dotenv()
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "http://localhost:5500",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 PLAID_CLIENT_ID = os.getenv("PLAID_CLIENT_ID")
 PLAID_SECRET = os.getenv("PLAID_SECRET")
@@ -34,12 +62,13 @@ products = []
 for product in PLAID_PRODUCTS:
     products.append(Products(product))
 
-app = FastAPI()
+# We store the access_token in memory - in production, store it in a secure
+# persistent data store.
+access_token = None
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def pretty_print_response(response):
+    print(json.dumps(response, indent=2, sort_keys=True, default=str))
 
 
 @app.post("/api/create_link_token")
@@ -58,3 +87,71 @@ async def create_link_token(request: Request):
     except plaid.ApiException as e:
         print(e)
         raise HTTPException(status_code=e.status, detail=json.loads(e.body))
+
+
+class PublicTokenForm(BaseModel):
+    public_token: str
+
+
+@app.post("/api/set_access_token")
+async def get_access_token(public_token: str = Body(..., embed=True)):
+    print(public_token)
+    global access_token, item_id, transfer_id
+    try:
+        exchange_request = ItemPublicTokenExchangeRequest(public_token=public_token)
+        exchange_response: ItemPublicTokenExchangeResponse = (
+            client.item_public_token_exchange(exchange_request)
+        )
+        access_token = exchange_response.access_token
+        item_id = exchange_response.item_id
+        return JSONResponse(content=exchange_response.to_dict())
+    except ApiException as e:
+        return JSONResponse(status_code=e.status, content=e.body)
+
+
+@app.post("/api/transactions")
+def get_transactions():
+    # Set cursor to empty to receive all historical updates
+    cursor = ""
+
+    # New transaction updates since "cursor"
+    added = []
+    modified = []
+    removed = []  # Removed transaction ids
+    has_more = True
+    try:
+        # Iterate through each page of new transaction updates for item
+        while has_more:
+            request = TransactionsSyncRequest(
+                access_token=access_token,
+                cursor=cursor,
+            )
+            response = client.transactions_sync(request).to_dict()
+            # Add this page of results
+            added.extend(response["added"])
+            modified.extend(response["modified"])
+            removed.extend(response["removed"])
+            has_more = response["has_more"]
+            # Update cursor to the next cursor
+            cursor = response["next_cursor"]
+            pretty_print_response(response)
+
+        # Return the 8 most recent transactions
+        latest_transactions = sorted(added, key=lambda t: t["date"])[-8:]
+        data = jsonable_encoder(latest_transactions)
+        with open("data/transactions.json", "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        return data
+    except ApiException as e:
+        return JSONResponse(status_code=e.status, content=e.body)
+
+
+@app.post("/api/auth")
+def get_auth():
+    try:
+        request = AuthGetRequest(access_token=access_token)
+        response = client.auth_get(request)
+        pretty_print_response(response.to_dict())
+        return jsonable_encoder(response.to_dict())
+    except ApiException as e:
+        return JSONResponse(status_code=e.status, content=e.body)
